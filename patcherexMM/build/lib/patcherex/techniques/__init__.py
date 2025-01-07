2f290edec44fdacdc1ba4bec2d2ff686ca6b42a4
sjaenich <jaenich@gmail.com>
2025-01-07 16:53:32 +0100

    added patcherex

---
+++ patcherexMM/build/lib/patcherex/techniques/__init__.py
@@ -0,0 +1,7 @@
+
+from .shadowstack import ShadowStack
+from .cpuid import CpuId
+from .shiftstack import ShiftStack
+from .adversarial import Adversarial
+from .binary_optimization import BinaryOptimization
+from .simple_ptr_enc import SimplePointerEncryption
