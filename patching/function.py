3fbdcdc3e83491dd1b0d965d8e3ce6d262247469
sjaenich <jaenich@gmail.com>
2025-04-24 17:19:36 +0200

    added error for function not found

--- patching/function.py
+++ patching/function.py
@@ -72,7 +72,8 @@ class FunctionPatch(Patching):
 
         if self.project_vuln.loader.find_symbol(self.patching_config.functionName) is None:
             print(self.patching_config.functionName + " not found in binary")
-            return
+            raise Exception("Function %s not found in binary", self.patching_config.functionName)
+
         self.entry_point_vuln = self.project_vuln.loader.find_symbol(self.patching_config.functionName).rebased_addr
         # TODO: Find a better option to get the end
         self.end_vuln = self.entry_point_vuln + self.project_vuln.loader.find_symbol(
