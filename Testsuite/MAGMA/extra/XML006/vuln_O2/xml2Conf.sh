8426fe5fe43c2de19f8ddf728efa854e97f6a1b9
jaenich <jaenich@plai.ifi.lmu.de>
2025-05-29 19:38:38 +0200

    removed Magma

--- Testsuite/MAGMA/extra/XML006/vuln_O2/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/jaenich/Magma/magma/targets/libxml2/repo/XML006/vuln_O2/lib"
-XML2_LIBS="-lxml2 -lz -llzma    -lm -lm -l:libz.so -l:liblzma.so -l:magma.o -l:libjpeg.so"
-XML2_INCLUDEDIR="-I/home/jaenich/Magma/magma/targets/libxml2/repo/XML006/vuln_O2/include/libxml2"
-MODULE_VERSION="xml2-2.9.12"
-
